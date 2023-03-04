67e313cf87f384ebab0e93e73112d82976514725
Marco Franssen <marco.franssen@gmail.com>
2023-03-04 14:37:43 +0100

    Add some post-install.sh scripts to display deploy statuses in some tests
    Signed-off-by: Marco Franssen <marco.franssen@gmail.com>

---
+++ .github/tests/spire-oidc-insecure/post-install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -x
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+NS="${SCRIPTPATH##*/}"
+
+k_wait=(kubectl wait --for condition=available --timeout 30s --namespace)
+k_rollout_status=(kubectl rollout status --watch --timeout 30s --namespace)
+
+cat <<EOF >>"$GITHUB_STEP_SUMMARY"
+### spire
+
+| workload | Status |
+| -------- | ------ |
+| spire-server | $("${k_rollout_status[@]}" "$NS" statefulset spire-server) |
+| spire-spiffe-csi-driver | $("${k_rollout_status[@]}" "$NS" daemonset spire-spiffe-csi-driver) |
+| spire-agent | $("${k_rollout_status[@]}" "$NS" daemonset spire-agent) |
+| spire-spiffe-oidc-discovery-provider | $("${k_wait[@]}" "$NS" deployments.apps spire-spiffe-oidc-discovery-provider) |
+EOF
--- .github/tests/upstream-authority-cert-manager/post-install.sh
+++ .github/tests/upstream-authority-cert-manager/post-install.sh
@@ -9,6 +9,29 @@ NS="${SCRIPTPATH##*/}"
 # shellcheck source=/dev/null
 source "${SCRIPTPATH}/../common.sh"
 
+k_wait=(kubectl wait --for condition=available --timeout 30s --namespace)
+k_rollout_status=(kubectl rollout status --watch --timeout 30s --namespace)
+
+cat <<EOF >>"$GITHUB_STEP_SUMMARY"
+### cert-manager
+
+| workload | Status |
+| -------- | ------ |
+| cert-manager | $("${k_wait[@]}" cert-manager deployments.apps cert-manager) |
+| cert-manager-cainjector | $("${k_wait[@]}" cert-manager deployments.apps cert-manager-cainjector) |
+| cert-manager-webhook | $("${k_wait[@]}" cert-manager deployments.apps cert-manager-webhook) |
+EOF
+
+cat <<EOF >>"$GITHUB_STEP_SUMMARY"
+### spire
+
+| workload | Status |
+| -------- | ------ |
+| spire-server | $("${k_rollout_status[@]}" "${NS}" statefulset spire-server) |
+| spire-spiffe-csi-driver | $("${k_rollout_status[@]}"  "${NS}" daemonset spire-spiffe-csi-driver) |
+| spire-agent | $("${k_rollout_status[@]}" "${NS}" daemonset spire-agent) |
+EOF
+
 if [ $1 -ne 0 ]; then
   get_namespace_details "${NS}"
 fi
